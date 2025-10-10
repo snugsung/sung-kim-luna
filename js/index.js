570fe791ddda34e7a01172f833932f30658f69a7
Sung Kim <sungkim@student.midlandstech.edu>
2025-10-10 10:45:00 -0400

    Insert copyright text in footer

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 const footer = document.createElement('footer');
 document.body.appendChild(footer);
+
+const today = new Date();
+const currentYear = today.getFullYear();
+
+const copyright = document.createElement('p');
+copyright.textContent = `© ${currentYear} Sung Kim`;
+footer.appendChild(copyright);
